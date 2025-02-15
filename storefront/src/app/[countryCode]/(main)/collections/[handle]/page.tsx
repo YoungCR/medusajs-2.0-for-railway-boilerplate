216180d0a7ed432cb149c543687911514269226a
Chris Young <chris@youngcr.com>
2025-02-15 17:56:42 -0600

    Changed hero text, nav title and other mentions of Medusa Store to Highball Hobbies

--- storefront/src/app/[countryCode]/(main)/collections/[handle]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/collections/[handle]/page.tsx
@@ -59,7 +59,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 
   const metadata = {
-    title: `${collection.title} | Medusa Store`,
+    title: `${collection.title} | Highball Hobbies`,
     description: `${collection.title} collection`,
   } as Metadata
 
